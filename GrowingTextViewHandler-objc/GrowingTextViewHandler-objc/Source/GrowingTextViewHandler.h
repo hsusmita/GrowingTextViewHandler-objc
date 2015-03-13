ecd214c611e56453eacce1f91d91f7c8f60fc4b8
hsusmita <susmita.horrow@gmail.com>
2015-03-13 12:50:00 +0530

    Growing Text View Handler added

--- GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.h
+++ GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.h
@@ -2,12 +2,18 @@
 //  GrowingTextViewHandler.h
 //  GrowingTextViewHandler-objc
 //
-//  Created by sah-fueled on 13/03/15.
+//  Created by hsusmita on 13/03/15.
 //  Copyright (c) 2015 hsusmita.com. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface GrowingTextViewHandler : NSObject
+
+@property (nonatomic, strong) UITextView *growingTextView;
+@property (nonatomic, assign) NSInteger maximumNumberOfLines;
+@property (nonatomic, assign) NSInteger minimumNumberOfLines;
+@property (nonatomic, assign) CGFloat animationDuration;
 
 @end
--- GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.m
+++ GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.m
@@ -2,12 +2,83 @@
 //  GrowingTextViewHandler.m
 //  GrowingTextViewHandler-objc
 //
-//  Created by sah-fueled on 13/03/15.
+//  Created by hsusmita on 13/03/15.
 //  Copyright (c) 2015 hsusmita.com. All rights reserved.
 //
 
 #import "GrowingTextViewHandler.h"
 
+@interface GrowingTextViewHandler()
+
+@property (nonatomic, strong) NSLayoutConstraint *heightConstraint;
+@property (nonatomic, assign) CGFloat initialHeight;
+
+@end
+
 @implementation GrowingTextViewHandler
+
+- (id)initWithTextView:(UITextView *)textView withHeightConstraint:(NSLayoutConstraint *)heightConstraint {
+  self = [super init];
+  if (self) {
+    self.growingTextView = textView;
+    self.heightConstraint = heightConstraint;
+    [self updateInitialHeightAndResize];
+  }
+  return self;
+}
+
+- (void)setMinimumNumberOfLines:(NSInteger)minimumNumberOfLines {
+  _minimumNumberOfLines = minimumNumberOfLines;
+  [self updateInitialHeightAndResize];
+}
+
+- (void)updateInitialHeightAndResize {
+  self.initialHeight = [self estimatedInitialHeight];
+  [self resizeTextViewWithAnimation:NO];
+}
+
+- (CGFloat)estimatedInitialHeight {
+  CGFloat caretHeight = [self.growingTextView caretRectForPosition:self.growingTextView.selectedTextRange.end].size.height;
+  CGFloat totalHeight = caretHeight * self.minimumNumberOfLines + self.growingTextView.textContainerInset.top + self.growingTextView.textContainerInset.bottom;
+  return fmax(totalHeight,self.growingTextView.frame.size.height);
+}
+
+- (void)resizeTextViewWithAnimation:(BOOL)animated {
+  NSInteger textViewNumberOfLines = self.currentNumberOfLines;
+  if (textViewNumberOfLines <= self.minimumNumberOfLines) {
+    [self updateVerticalAlignmentWithHeight:self.initialHeight animated:animated];
+    [self.growingTextView setContentOffset:CGPointZero animated:YES];
+  }else if ((textViewNumberOfLines > self.minimumNumberOfLines) && (textViewNumberOfLines <= self.maximumNumberOfLines)) {
+    CGFloat currentHeight = [self currentHeight];
+    CGFloat verticalAlignmentConstant = (currentHeight > self.initialHeight) ? currentHeight : self.initialHeight;
+    [self updateVerticalAlignmentWithHeight:verticalAlignmentConstant animated:animated];
+    [self.growingTextView setContentOffset:CGPointZero animated:YES];
+  }
+}
+
+- (CGFloat)currentHeight {
+  return self.growingTextView.contentSize.height;
+}
+
+- (NSInteger) currentNumberOfLines  {
+  CGFloat caretHeight = [self.growingTextView caretRectForPosition:self.growingTextView.selectedTextRange.end].size.height;
+  CGFloat totalHeight = self.growingTextView.contentSize.height + self.growingTextView.textContainerInset.top + self.growingTextView.textContainerInset.bottom;
+  NSInteger numberOfLines = (totalHeight/caretHeight) - 1;
+  return numberOfLines;
+}
+
+- (void)updateVerticalAlignmentWithHeight:(CGFloat)height animated:(BOOL)animated {
+  self.heightConstraint.constant = height;
+  if (animated == true) {
+    [UIView animateWithDuration:self.animationDuration
+                     animations:^{
+      [self.growingTextView.superview layoutIfNeeded];
+    }
+                     completion:nil];
+  }
+   else {
+    [self.growingTextView.superview layoutIfNeeded];
+  }
+}
 
 @end
