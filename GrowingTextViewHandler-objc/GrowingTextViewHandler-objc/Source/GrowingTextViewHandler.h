e948080e0f57796be68d4efef39f03ba2e5aac51
hsusmita <susmita.horrow@gmail.com>
2015-06-05 16:19:40 +0530

    Documentation done

--- GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.h
+++ GrowingTextViewHandler-objc/GrowingTextViewHandler-objc/Source/GrowingTextViewHandler.h
@@ -9,13 +9,36 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+/**
+ An NSObject subclass to handle resizing of UITextView as the user types in. The textview resizes as long as the number of lines lies between specified minimum and maximum number of lines. This class calculates total size of UITextView text and adjusts the height constraint of that UITextView. You need to provide height constraint to UITextView.
+ */
+
 @interface GrowingTextViewHandler : NSObject
 
 @property (nonatomic, strong) UITextView *growingTextView;
 @property (nonatomic, assign) CGFloat animationDuration;
 
+
+/** Returns an instance of GrowingTextViewHandler
+ @param textView The UITextView which needs to be resized
+ @param heightConstraint The height constraint of textview
+ */
+
 - (id)initWithTextView:(UITextView *)textView withHeightConstraint:(NSLayoutConstraint *)heightConstraint;
-- (void)resizeTextViewWithAnimation:(BOOL)animated;
+
+/** Limits resizing of UITextView between minimumNumberOfLines and maximumNumberOfLines
+ @param minimumNumberOfLines Lower limit on number of lines
+ @param maximumNumberOfLines Upper limit on number of lines
+ */
+
 - (void)updateMinimumNumberOfLines:(NSInteger)minimumNumberOfLines andMaximumNumberOfLine:(NSInteger)maximumNumberOfLines;
+
+
+/** Resizes the textView according to the amount of text. 
+ @param animated Specify YES if you want to animate the size change of UITextView or NO if you don't
+ */
+
+- (void)resizeTextViewWithAnimation:(BOOL)animated;
+
 
 @end
